62eb133082d2933609cdd2e52737f3d95543fba3
Mohamed Assem <mohamedassem663@gmail.com>
2025-06-14 20:10:21 +0300

    creat app colors &some change in custom app bar

--- lib/componants/custom_appbar.dart
+++ lib/componants/custom_appbar.dart
@@ -1,53 +1,48 @@
+import 'package:fashion/core/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(60.0);
+  final bool isBlackk;
 
   // Constructor
-  const CustomAppbar({super.key});
+  const CustomAppbar({super.key, required this.isBlackk});
 
   @override
   Widget build(BuildContext context) {
+    bool isBlack = isBlackk;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor:isBlack?AppColors.primary :Colors.white,
         elevation: 0,
         
       leading:SvgPicture.asset("assets/svgs/Menu.svg",
-            width: 10,
-            height: 10,
-            color: Colors.white,
+           
+            color:isBlack ? Colors.white:AppColors.primary,
             
           ),
+          leadingWidth: 25,
       centerTitle: true,
         title: SvgPicture.asset(
           "assets/logo/logo-bg.svg",
-          width: 100,
-          height: 40,
-          color: Colors.white,
+        
+         color:isBlack ? Colors.white:AppColors.primary,
         ),
         actions: [
           SvgPicture.asset("assets/svgs/Search.svg",height: 30, width: 30, ),
-          const SizedBox(width: 15),
+          const SizedBox(width: 18),
           SvgPicture.asset(
             "assets/svgs/shopping bag.svg",
             height: 30,
             width: 30,
-            color: Colors.white,
-          ),
-          const SizedBox(width: 15),
+color:isBlack ? Colors.white:AppColors.primary,          ),
+          const SizedBox(width: 18),
         ],
       ),
     );
     
   }
 }
-
-//  IconButton(
-//       icon: const Icon(Icons.menu, color: Colors.black),
-//       onPressed: () {
-//         // Action for menu button
-//       },
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:fashion/componants/custom_appbar.dart';
+import 'package:fashion/core/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -7,11 +8,30 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: CustomAppbar(),
+    return Scaffold(backgroundColor: AppColors.primary,appBar: CustomAppbar(
+      isBlackk: true,
+    ),
     body:Stack(
       children: [
-        Center(child: SvgPicture.asset("assets/texts/10.svg"))
-      ]));
+        Positioned(
+          top: 10,
+          left: 0,
+          right: 0,
+          child: SvgPicture.asset("assets/texts/10.svg")),
+             Positioned(
+            top: 40,
+          left: 0,
+          right: 0,
+          child: SvgPicture.asset("assets/texts/October.svg")),
+        Positioned(
+            top:85 ,
+          left: 0,
+          right: 0,
+          child: SvgPicture.asset("assets/texts/Collection.svg")),
+     
+      ],
+      ),
+      );
       
   }
 }
